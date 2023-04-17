6a727227ad2bcb25c889987993c4e5ad66d316bf
zecky1120 <takahide1120@gmail.com>
2023-04-17 21:52:48 +0900

    カレンダープログラムの再提出 Part2

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,18 +8,16 @@ opt = OptionParser.new
 opt.on('-y VAL', Integer, '年を入力しください（数字のみ）') {|v| option[:y] = v }
 opt.on('-m VAL', Integer, '月を入力しください（数字のみ）') {|v| option[:m] = v }
 opt.parse(ARGV)
-start_day = Date.new(option[:y], option[:m], 1)
-last_day = Date.new(option[:y], option[:m], -1)
+start_date = Date.new(option[:y], option[:m], 1)
+last_date = Date.new(option[:y], option[:m], -1)
 
-puts "#{option[:m]}月 #{option[:y]}".center(20)
-puts '日 月 火 水 木 金 土'.center(16)
+puts "#{option[:m]}月 #{option[:y]}".center(19)
+puts '日 月 火 水 木 金 土'
 
-week_number = start_day.wday
-space = '   ' * week_number
-print space
+print '   ' * start_date.wday
 
-(start_day..last_day).each do |day|
-  print day.mday.to_s.rjust(3)
+(start_date..last_date).each do |day|
+  print day.mday.to_s.center(3)
   if day.saturday?
     print "\n"
   end
