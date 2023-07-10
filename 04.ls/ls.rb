6c1aced5dc449deaad0123c4e89caa6ba6e7ee64
zecky1120 <takahide1120@gmail.com>
2023-07-10 19:06:32 +0900

    lsコマンド- 再提出

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 def find_files
-  Dir.glob('*').sort!
+  Dir.glob('*')
 end
 
 def string_max_size
   find_files.map(&:size).max + 2
 end
 
-def display_files_list(column)
+def display_files_list(col)
   number_of_files = find_files.size
-  number_of_columns = number_of_files.ceildiv(column)
+  number_of_columns = number_of_files.ceildiv(col)
   column_conditioning_array = find_files.each_slice(number_of_columns).to_a
   adjustment_array =
     column_conditioning_array.each do |item|
@@ -18,7 +18,7 @@ def display_files_list(column)
     end
   adjustment_array.transpose.each do |file_names|
     file_names.each do |file_name|
-      printf("%-#{string_max_size}s", file_name)
+      print file_name.to_s.ljust(string_max_size)
     end
     puts
   end
