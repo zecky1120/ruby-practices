c592e4d6572c69d56a6457a8b87d0fd8efb98a1c
zecky1120 <takahide1120@gmail.com>
2023-07-17 21:16:34 +0900

    lsコマンド オプションあり -l

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,27 +1,43 @@
 # frozen_string_literal: true
 
-def find_files
-  Dir.glob('*')
-end
+require 'optparse'
 
-def string_max_size
-  find_files.map(&:size).max + 2
-end
+class AppOption
+  def initialize
+    OptionParser.new do |opt|
+      opt.on('-l') { find_files }
+      opt.parse(ARGV)
+    end
+  end
 
-def display_files_list(col)
-  number_of_files = find_files.size
-  number_of_columns = number_of_files.ceildiv(col)
-  column_conditioning_array = find_files.each_slice(number_of_columns).to_a
-  adjustment_array =
-    column_conditioning_array.each do |item|
-      item << nil while item.size < number_of_columns
+  def find_files
+    if ARGV[0] == '-l'
+      Dir.glob('*', File::FNM_DOTMATCH)
+    else
+      Dir.glob('*')
     end
-  adjustment_array.transpose.each do |file_names|
-    file_names.each do |file_name|
-      print file_name.to_s.ljust(string_max_size)
+  end
+
+  def string_max_size
+    find_files.map(&:size).max + 2
+  end
+
+  def display_files_list(max_colums)
+    number_of_files = find_files.size
+    number_of_columns = number_of_files.ceildiv(max_colums)
+    column_conditioning_array = find_files.each_slice(number_of_columns).to_a
+    adjustment_array =
+      column_conditioning_array.each do |item|
+        item << nil while item.size < number_of_columns
+      end
+    adjustment_array.transpose.each do |file_names|
+      file_names.each do |file_name|
+        print file_name.to_s.ljust(string_max_size)
+      end
+      puts
     end
-    puts
   end
 end
 
-display_files_list(3)
+option = AppOption.new
+option.display_files_list(3)
