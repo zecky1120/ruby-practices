8701f15a4223f6a513695c52353c6b8c71ad7da0
zecky1120 <takahide1120@gmail.com>
2023-12-18 21:53:06 +0900

    空白スペースをすべて揃え、メソッド名や、コードを再構築した

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -2,15 +2,18 @@
 
 require 'optparse'
 
-def main(options)
+def main
+  options = parse_options
   options = { l: true, w: true, c: true } if options.empty?
   paths = ARGV
-  wc_count_files = build_wc_counts(paths:)
-  wc_count_files.map { |wc_count_file| display_wc(count_file: wc_count_file, options:, paths:) }
-  display_total_count(wc_count_files:, options:, paths:) if paths.size >= 2
+  wc_count_files = build_wc_counts(paths)
+  wc_count_files.each do |wc_count_file|
+    display_wc(wc_count_file, options, paths)
+  end
+  display_total_count(wc_count_files, options, paths) if paths.size >= 2
 end
 
-def options
+def parse_options
   options = {}
   opt = OptionParser.new
   opt.on('-l') { options[:l] = true }
@@ -20,61 +23,52 @@ def options
   options
 end
 
-def build_wc_counts(paths:)
+def build_wc_counts(paths)
   if paths.empty?
-    [build_wc(lines: $stdin.read)]
+    [build_wc(files: $stdin.read)]
   else
     paths.map do |path|
-      build_wc(lines: File.read(path), path:)
+      build_wc(files: File.read(path), path:)
     end
   end
 end
 
-def build_wc(lines:, path: '')
+def build_wc(files:, path: '')
   {
-    lines: lines.count("\n"),
-    words: lines.split(/\s+/).count,
-    file_sizes: lines.bytesize,
+    lines: files.count("\n"),
+    words: files.split(/\s+/).count,
+    file_sizes: files.bytesize,
     path:
   }
 end
 
-def build_count_size(paths:, options:)
+def build_count_size(paths, options)
   count_ary = []
-  string_sizes = build_wc_counts(paths:)
-  string_sizes.each do |string_size|
-    count_ary << string_size[:lines] if options[:l]
-    count_ary << string_size[:words] if options[:w]
-    count_ary << string_size[:file_sizes] if options[:c]
+  wc_count_files.each do |wc_count_file|
+    count_ary << wc_count_file[:lines] if options[:l]
+    count_ary << wc_count_file[:words] if options[:w]
+    count_ary << wc_count_file[:file_sizes] if options[:c]
   end
   count_ary
 end
 
-def build_align_width(paths:, options:)
-  string_size_counts = build_count_size(paths:, options:)
-  fixed_max_width =
-    if paths.size <= 1 && options.size <= 1
-      0
-    elsif paths.empty? && options.size >= 2
-      7
-    else
-      string_size_counts.max.to_s.size
-    end
-  {
-    max_line: fixed_max_width,
-    max_word: fixed_max_width + 1,
-    max_file_size: fixed_max_width + 1
-  }
+def build_align_width(paths)
+  string_size_counts = build_wc_counts(paths)[0].fetch(:file_sizes).to_s.size
+  paths.empty? ? 7 : string_size_counts
 end
 
-def display_wc(count_file:, options:, paths:)
-  string_size_count = build_align_width(paths:, options:)
+def display_wc(count_file, options, paths)
+  string_size_count = build_align_width(paths)
   count_files_ary = []
-  count_files_ary << count_file[:lines].to_s.rjust(string_size_count[:max_line]) if options[:l]
-  count_files_ary << count_file[:words].to_s.rjust(string_size_count[:max_word]) if options[:w]
-  count_files_ary << count_file[:file_sizes].to_s.rjust(string_size_count[:max_file_size]) if options[:c]
-  count_files_ary << " #{count_file[:path]}"
-  puts count_files_ary.join
+  count_files_ary << "#{count_file[:lines].to_s.rjust(string_size_count)} " if options[:l]
+  count_files_ary << "#{count_file[:words].to_s.rjust(string_size_count)} " if options[:w]
+  count_files_ary << "#{count_file[:file_sizes].to_s.rjust(string_size_count)} " if options[:c]
+  count_files_ary << count_file[:path].to_s
+  if paths.size <= 1 && options.size <= 1
+    puts count_files_ary.join.strip
+  else
+    puts count_files_ary.join
+  end
 end
 
 def build_wc_total_counts(wc_count_files)
@@ -86,9 +80,9 @@ def build_wc_total_counts(wc_count_files)
   }
 end
 
-def display_total_count(wc_count_files:, options:, paths:)
+def display_total_count(wc_count_files, options, paths)
   total_count_file = build_wc_total_counts(wc_count_files)
-  display_wc(count_file: total_count_file, options:, paths:)
+  display_wc(total_count_file, options, paths)
 end
 
-main(options)
+main
