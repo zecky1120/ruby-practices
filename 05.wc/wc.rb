dbac73a2ed5f28abc10f69ae337016e849b130c8
zecky1120 <takahide1120@gmail.com>
2023-12-21 21:30:53 +0900

    再調整

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -4,13 +4,12 @@ require 'optparse'
 
 def main
   options = parse_options
-  options = { l: true, w: true, c: true } if options.empty?
   paths = ARGV
-  wc_count_files = build_wc_counts(paths)
-  wc_count_files.each do |wc_count_file|
-    display_wc(wc_count_file, options, paths)
+  wc_counts = build_wc_counts(paths)
+  wc_counts.each do |wc_count|
+    display_wc(wc_count, options, paths)
   end
-  display_total_count(wc_count_files, options, paths) if paths.size >= 2
+  wc_total_count(wc_counts, options, paths) if paths.size >= 2
 end
 
 def parse_options
@@ -20,6 +19,7 @@ def parse_options
   opt.on('-w') { options[:w] = true }
   opt.on('-c') { options[:c] = true }
   opt.permute!(ARGV)
+  options = { l: true, w: true, c: true } if options.empty?
   options
 end
 
@@ -42,36 +42,26 @@ def build_wc(files:, path: '')
   }
 end
 
-def build_count_size(options)
-  count_ary = []
-  wc_count_files.each do |wc_count_file|
-    count_ary << wc_count_file[:lines] if options[:l]
-    count_ary << wc_count_file[:words] if options[:w]
-    count_ary << wc_count_file[:file_sizes] if options[:c]
+def build_align_width(paths, options)
+  filtered_count_data = build_wc_counts(paths).max_by { |v| v[:file_sizes] }
+  max_width = filtered_count_data[:file_sizes].to_s.size
+  if paths.empty?
+    7
+  elsif paths.size <= 1 && options.size <= 1
+    0
+  else
+    max_width
   end
-  count_ary
-end
-
-def build_align_width(paths)
-  extract_file_sizes = build_wc_counts(paths).map do |count|
-    count[:file_sizes]
-  end
-  string_max_size_count = extract_file_sizes.max.to_s.size
-  paths.empty? ? 7 : string_max_size_count
 end
 
 def display_wc(count_file, options, paths)
-  string_size_count = build_align_width(paths)
-  count_files_ary = []
-  count_files_ary << "#{count_file[:lines].to_s.rjust(string_size_count)} " if options[:l]
-  count_files_ary << "#{count_file[:words].to_s.rjust(string_size_count)} " if options[:w]
-  count_files_ary << "#{count_file[:file_sizes].to_s.rjust(string_size_count)} " if options[:c]
-  count_files_ary << count_file[:path].to_s
-  if paths.size <= 1 && options.size <= 1
-    puts count_files_ary.join.strip
-  else
-    puts count_files_ary.join
-  end
+  align_width = build_align_width(paths, options)
+  count_files = []
+  count_files << count_file[:lines].to_s.rjust(align_width) if options[:l]
+  count_files << count_file[:words].to_s.rjust(align_width) if options[:w]
+  count_files << count_file[:file_sizes].to_s.rjust(align_width) if options[:c]
+  count_files << count_file[:path].to_s
+  puts count_files.join(' ')
 end
 
 def build_wc_total_counts(wc_count_files)
@@ -83,7 +73,7 @@ def build_wc_total_counts(wc_count_files)
   }
 end
 
-def display_total_count(wc_count_files, options, paths)
+def wc_total_count(wc_count_files, options, paths)
   total_count_file = build_wc_total_counts(wc_count_files)
   display_wc(total_count_file, options, paths)
 end
