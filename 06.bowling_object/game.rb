25ec76f663d7acef0a78be2b58d41648e992f293
zecky1120 <takahide1120@gmail.com>
2024-08-27 20:33:15 +0900

    fix: メソッド名を変更

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -8,49 +8,49 @@ class Game
     @shot = shot
   end
 
-  def shot_scores
+  def shots
     @shot.split(',')
   end
 
   def main
     @frames = frames
     total = 0
-    (0..8).each do |i|
-      frame = Frame.new(@frames[i])
+    (0..8).each do |n|
+      frame = Frame.new(@frames[n])
       total += frame.scores
-      total += add_bonus(frame, i)
-      add_bonus(frame, i)
+      total += add_bonus(frame, n)
+      add_bonus(frame, n)
     end
     total + @frames.last.sum
   end
 
   def frames
-    shots = shot_scores.map { |shot| Shot.new(shot).score }
+    scores = shots.map { |shot| Shot.new(shot).score }
     frame = []
     frames = []
-    shots.each do |s|
-      frame << s
+    scores.each do |score|
+      frame << score
       if frames.length < 10
-        if frame.length >= 2 || s == 10
+        if frame.length >= 2 || score == 10
           frames << frame.dup
           frame.clear
         end
       else
-        frames.last << s
+        frames.last << score
       end
     end
     frames
   end
 
-  def add_bonus(frame, i)
+  def add_bonus(frame, n)
     bonus = 0
-    frame_size = frames[i.succ].size >= 2
-    next_shot = frames[i.succ].first(2).sum
-    next_frame = frame_size ? next_shot : next_shot + frames[i.succ + 1][0]
+    frame_size = frames[n.succ].size >= 2
+    next_shot = frames[n.succ].first(2).sum
+    next_frame = frame_size ? next_shot : next_shot + frames[n.succ + 1][0]
     if frame.strike?
       bonus += next_frame
     elsif frame.spare?
-      bonus += frames[i + 1][0]
+      bonus += frames[n + 1][0]
     else
       0
     end
