65f40237af69fbf33e9e65051143e5a18bb69aaf
zecky1120 <takahide1120@gmail.com>
2024-10-24 21:52:55 +0900

    fix: Frameクラスにshotsからshotに修正&&かつ、非プリミティブなオブジェクトに修正

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -3,21 +3,21 @@
 require './shot'
 
 class Frame
-  attr_reader :shots
+  attr_reader :shot
 
   def initialize(rolls)
-    @shots = rolls.map { |roll| Shot.new(roll).score }
+    @shot = rolls.map { |roll| Shot.new(roll) }
   end
 
   def scores
-    @shots.sum
+    @shot.sum(&:score)
   end
 
   def strike?
-    @shots[0] == 10
+    @shot[0].score == 10
   end
 
   def spare?
-    @shots[0] + @shots[1] == 10
+    @shot[0].score + @shot[1].score == 10
   end
 end
