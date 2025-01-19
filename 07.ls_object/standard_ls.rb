6151d534cb15b6e88bed9576eea560706b4d022e
zecky1120 <takahide1120@gmail.com>
2025-01-19 17:41:42 +0900

    fix: COLUMNSの数値を変更

--- 07.ls_object/standard_ls.rb
+++ 07.ls_object/standard_ls.rb
@@ -3,17 +3,16 @@
 require_relative 'base_ls'
 
 class StandardLS < BaseLS
-  COLUMNS = 3
+  COLUMNS = 5
 
-  def main
-    files = find_files
-    display_files(files)
+  def display_file_list
+    formatted_file_list(find_files)
   end
 
-  def display_files(files)
-    column_max_size = files.map(&:size).max
-    number_of_column = files.size.ceildiv(COLUMNS)
-    columns = files.each_slice(number_of_column).to_a
+  def formatted_file_list(find_files)
+    column_max_size = find_files.map(&:size).max
+    number_of_column = find_files.size.ceildiv(COLUMNS)
+    columns = find_files.each_slice(number_of_column).to_a
     columns[0].zip(*columns[1..]).map do |row|
       row.map { |file| file.to_s.ljust(column_max_size + 2) }.join.rstrip
     end.join("\n")
