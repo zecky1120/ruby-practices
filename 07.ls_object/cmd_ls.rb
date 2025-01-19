70f0fc6386b18f56248d9e348ffcb135139324ce
zecky1120 <takahide1120@gmail.com>
2025-01-19 17:40:55 +0900

    fix: long_optionの処理を追加

--- 07.ls_object/cmd_ls.rb
+++ 07.ls_object/cmd_ls.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
 require_relative 'standard_ls'
+require_relative 'long_option_ls'
 
 class Command
   def initialize(option)
     @option = option
   end
 
-  def main
+  def output
     option = BaseLS.new(@option).parse_params(@option)
-    if option[:l]
-      puts 'longオプションを表示'
-    else
-      StandardLS.new(@option).main
-    end
+    option[:l] ? LongOptionLS.new(@option).display_long_file_list : StandardLS.new(@option).display_file_list
   end
 end
 
 cmd = Command.new(ARGV)
-puts cmd.main
+puts cmd.output
