a8652626357cfb7c934c6428a42fd6209283ecac
zecky1120 <takahide1120@gmail.com>
2025-02-06 21:39:23 +0900

    add: command.rbを追加

---
+++ 07.ls_object/command.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require_relative 'file_list'
+require_relative 'long_format'
+
+class Command
+  def initialize(option)
+    @params = parse_params(option)
+  end
+
+  def output
+    @params[:l] ? LongFormat.new(files).display : FileList.new(files).display
+  end
+
+  private
+
+  def parse_params(option)
+    params = {}
+    opt = OptionParser.new
+    opt.on('-a') { |a| params[:a] = a }
+    opt.on('-r') { |r| params[:r] = r }
+    opt.on('-l') { |l| params[:l] = l }
+    opt.parse(option)
+    params
+  end
+
+  def files
+    flags = @params[:a] ? File::FNM_DOTMATCH : 0
+    dir = Dir.glob('*', flags)
+    @params[:r] ? dir.reverse : dir
+  end
+end
