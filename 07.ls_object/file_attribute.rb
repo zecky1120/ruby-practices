7c92ae37e8201b72dece097b6ea49f6442622dd4
zecky1120 <takahide1120@gmail.com>
2025-02-11 20:05:25 +0900

    fix: データを処理するものとフォーマットするものを分けて実装

--- 07.ls_object/file_attribute.rb
+++ 07.ls_object/file_attribute.rb
@@ -8,21 +8,6 @@ class FileAttribute
     @file_stat = File::Stat.new(@file)
   end
 
-  def build_rowfile
-    {
-      blocks: @file_stat.blocks,
-      filemode: convert_filetype,
-      nlink: @file_stat.nlink,
-      owner: Etc.getpwuid(@file_stat.uid).name,
-      group: Etc.getgrgid(@file_stat.gid).name,
-      size: @file_stat.size,
-      mtime: last_update,
-      basename: File.basename(@file)
-    }
-  end
-
-  private
-
   GET_FILE_FTYPE = {
     'fifo' => 'p',
     'characterSpecial' => 'c',
@@ -33,51 +18,43 @@ class FileAttribute
     'socket' => 's'
   }.freeze
 
-  GET_FILE_PERMISSION = {
-    '0' => '---',
-    '1' => '--x',
-    '2' => '-w-',
-    '3' => '-wx',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx'
-  }.freeze
-
-  def convert_filetype
-    ftype = GET_FILE_FTYPE[@file_stat.ftype]
-    octal = @file_stat.mode.to_s(8)
-    slice_octal = octal.slice(-3, 3)
-    permissions = get_special_authority(octal, slice_octal).join
-    "#{ftype}#{permissions}"
+  def blocks
+    @file_stat.blocks
   end
 
-  def get_special_authority(octal, slice_octal)
-    slice_octal.each_char.with_index.map do |oc, index|
-      perm = GET_FILE_PERMISSION[oc]
-      apply_special_permission(octal, perm, index)
-    end
+  def ftype
+    GET_FILE_FTYPE[@file_stat.ftype]
   end
 
-  def apply_special_permission(octal, perm, index)
-    special_bit = octal.slice(-4)
-    case [special_bit, index]
-    when ['1', 2]
-      perm[-1] == 'x' ? perm.sub(/.$/, 't') : perm.sub(/.$/, 'T')
-    when ['2', 1]
-      perm[-1] == 'x' ? perm.sub(/.$/, 's') : perm.sub(/.$/, 'S')
-    when ['4', 0]
-      perm[-1] == 'x' ? perm.sub(/.$/, 's') : perm.sub(/.$/, 'S')
-    else
-      perm
-    end
+  def mode
+    @file_stat.mode.to_s(8)
   end
 
-  def last_update
+  def nlink
+    @file_stat.nlink
+  end
+
+  def owner
+    Etc.getpwuid(@file_stat.uid).name
+  end
+
+  def group
+    Etc.getgrgid(@file_stat.gid).name
+  end
+
+  def size
+    @file_stat.size
+  end
+
+  def mtime
     current_time = Time.now
     time_stamp = @file_stat.mtime
     half_year_ago = current_time - 15_552_000
     time_or_year = time_stamp >= half_year_ago ? time_stamp.strftime('%H:%M') : time_stamp.strftime('%Y')
     "#{time_stamp.strftime('%_m %_d').rjust(5)} #{time_or_year.rjust(5)}"
+  end
+
+  def basename
+    File.basename(@file)
   end
 end
--- 07.ls_object/long_format.rb
+++ 07.ls_object/long_format.rb
@@ -3,12 +3,23 @@
 require_relative 'file_attribute'
 
 class LongFormat
+  GET_FILE_PERMISSION = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
+
   def initialize(files)
     @files = files
   end
 
   def display
-    rows = @files.map { |file| FileAttribute.new(file).build_rowfile }
+    rows = @files.map { |file| generate_format_row(FileAttribute.new(file)) }
     total_block = rows.sum { |row| row[:blocks] }
     total = "total #{total_block}"
     body = format_rows(rows)
@@ -16,6 +27,45 @@ class LongFormat
   end
 
   private
+
+  def generate_format_row(file)
+    {
+      blocks: file.blocks,
+      filemode: "#{file.ftype}#{file_permissions(file.mode)}",
+      nlink: file.nlink,
+      owner: file.owner,
+      group: file.group,
+      size: file.size,
+      mtime: file.mtime,
+      basename: file.basename
+    }
+  end
+
+  def file_permissions(octal)
+    slice_octal = octal.slice(-3, 3)
+    get_special_authority(octal, slice_octal).join
+  end
+
+  def get_special_authority(octal, slice_octal)
+    slice_octal.each_char.with_index.map do |oc, index|
+      perm = GET_FILE_PERMISSION[oc]
+      apply_special_permission(octal, perm, index)
+    end
+  end
+
+  def apply_special_permission(octal, perm, index)
+    special_bit = octal.slice(-4)
+    case [special_bit, index]
+    when ['1', 2]
+      perm[-1] == 'x' ? perm.sub(/.$/, 't') : perm.sub(/.$/, 'T')
+    when ['2', 1]
+      perm[-1] == 'x' ? perm.sub(/.$/, 's') : perm.sub(/.$/, 'S')
+    when ['4', 0]
+      perm[-1] == 'x' ? perm.sub(/.$/, 's') : perm.sub(/.$/, 'S')
+    else
+      perm
+    end
+  end
 
   def format_rows(rows)
     max_sizes = %i[nlink owner group size].map do |key|
